8380223fb69e7a309d5021d7c3c1695f363c398b
vahid <jalili.vahid@gmail.com>
2018-02-15 13:22:27 -0800

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+Package install information.
+"""
+
+import ast
+import os
+import re
+
+from distutils.core import setup
+
+prog = re.compile(r'__version__\s*=\s*(.+)')
+with open(os.path.join('cloud_authz', '__init__.py')) as file:
+    for line in file:
+        match_object = prog.match(line)
+        if match_object:
+            version = ast.literal_eval(match_object.group(1))
+            break
+
+REQ = [
+    'requests == 2.18.4'
+]
+
+setup(
+    name='cloud-authz',
+    version=version,
+    description='Implements means of authorization delegation on cloud-based '
+                'resource providers without sharing credentials.',
+    author='Vahid Jalili',
+    url='https://github.com/galaxyproject/cloud-authz',
+    install_requires=REQ,
+    license='MIT'
+)
