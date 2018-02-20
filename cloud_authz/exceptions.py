6d6b8d387374ba08a0e337b5d958bab232001fb5
vahid <jalili.vahid@gmail.com>
2018-02-20 09:14:20 -0800

    Add exceptions.py

---
+++ cloud_authz/exceptions.py
@@ -0,0 +1,31 @@
+"""
+Wraps exceptions for authorizing and/or exchanging tokens.
+"""
+
+
+class CloudAuthzBaseException(ValueError):
+    """
+    Base class for all the exceptions thrown (or relayed) in CloudAuthnz.
+    """
+
+    pass
+
+
+class InvalidTokenException(CloudAuthzBaseException):
+    """
+    Represents errors that occur during ID token utilization.
+    """
+
+    def __str__(self):
+        return "The ID token is invalid."
+
+
+class ExpiredTokenException(CloudAuthzBaseException):
+    """
+    Represents errors that occur when utilizing expired tokens.
+    """
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "Token expired. Provider message: {}".format(self.message)
