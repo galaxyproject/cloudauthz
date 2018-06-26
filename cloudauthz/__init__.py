8dca5cbe22a3bca8d48db7467d19add2c5bc22c7
vjalili <jalili.vahid@gmail.com>
2018-06-26 11:19:09 -0700

    Add Azure to cloudauthz.

--- cloudauthz/__init__.py
+++ cloudauthz/__init__.py
@@ -5,6 +5,7 @@ Implements means of delegating access to cloud-based resources.
 __version__ = "0.0.2"
 
 from providers import aws
+from providers import azure
 
 
 class CloudAuthz:
@@ -24,4 +25,14 @@ class CloudAuthz:
                 config['role_arn'],
                 config.get('role_session_name', 900),
                 config.get('role_session_name', 'cloudauthz'))
-        raise NotImplementedError("Authorization flow for the provider `{}` is not implemented.".format(provider))
+        elif provider.lower() == "azure":
+            authz = azure.Authorize()
+            if "tenant_id" not in config:
+                raise KeyError("`tenant_id` is not provided.")
+            if "client_id" not in config:
+                raise KeyError("`client_id` is not provided.")
+            if "client_secret" not in config:
+                raise KeyError("`client_secret` is not provided.")
+            return authz.get_credentials(config["tenant_id"], config["client_id"], config["client_secret"])
+        else:
+            raise NotImplementedError("Authorization flow for the provider `{}` is not implemented.".format(provider))
